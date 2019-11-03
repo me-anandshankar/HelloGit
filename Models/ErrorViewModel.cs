beb4b575f0ca0e044e1eda090c8ba1696b9b6ead
Anand Shankar <v-ansha1@microsoft.com>
2019-11-03 17:38:10 +0530

    dotnet core mvc new app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
